11b0a86b1cb254dbfb5cb47f92f75ea3f8c41695
WilsonnnTan <wilsonangelietann@gmail.com>
2026-03-10 22:31:44 +0700

    choreL setup linter, prettier, lint-staged and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,24 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  eslintConfigPrettier,
   globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'generated/**',
   ]),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
 ]);
 
 export default eslintConfig;
