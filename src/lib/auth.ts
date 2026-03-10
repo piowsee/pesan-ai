f1076dc30dbd99f72ce7205b9cae43a6bf4f7a97
WilsonnnTan <wilsonangelietann@gmail.com>
2026-03-10 22:34:15 +0700

    chore: apply prettier formatting across codebase

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,9 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import prisma from "@/lib/prisma"
+import prisma from '@/lib/prisma';
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "postgresql",
+    provider: 'postgresql',
   }),
 });
