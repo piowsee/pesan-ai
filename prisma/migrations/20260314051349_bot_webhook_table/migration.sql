a3592cc931b4cde1ae5cc2fd737fa601b3a37d66
WilsonnnTan <wilsonangelietann@gmail.com>
2026-03-14 12:14:02 +0700

    feat(db): add bot webhook table

---
+++ prisma/migrations/20260314051349_bot_webhook_table/migration.sql
@@ -0,0 +1,25 @@
+-- AlterTable
+ALTER TABLE "phone_number" ADD COLUMN     "botWebhookId" TEXT;
+
+-- CreateTable
+CREATE TABLE "bot_webhook" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "webhookUrl" TEXT NOT NULL,
+    "description" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "bot_webhook_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "idx_bot_webhooks_userId" ON "bot_webhook"("userId");
+
+-- AddForeignKey
+ALTER TABLE "phone_number" ADD CONSTRAINT "phone_number_botWebhookId_fkey" FOREIGN KEY ("botWebhookId") REFERENCES "bot_webhook"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bot_webhook" ADD CONSTRAINT "bot_webhook_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
