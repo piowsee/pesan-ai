b8a6388a9b75f8d29b1f5ee6b1e0243bd74e72b8
WilsonnnTan <wilsonangelietann@gmail.com>
2026-03-14 12:11:25 +0700

    feat(db): add phone number table

---
+++ prisma/migrations/20260314051110_phone_number_table/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "phone_number" (
+    "id" TEXT NOT NULL,
+    "phoneNumberId" TEXT NOT NULL,
+    "displayPhoneNumber" TEXT NOT NULL,
+    "verifiedName" TEXT,
+    "codeVerificationStatus" TEXT NOT NULL DEFAULT 'VERIFIED',
+    "qualityRating" TEXT,
+    "botEnabled" BOOLEAN NOT NULL DEFAULT true,
+    "unreadCount" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "wabaId" TEXT NOT NULL,
+
+    CONSTRAINT "phone_number_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "phone_number_phoneNumberId_key" ON "phone_number"("phoneNumberId");
+
+-- CreateIndex
+CREATE INDEX "idx_phone_number_wabaId" ON "phone_number"("wabaId");
+
+-- AddForeignKey
+ALTER TABLE "phone_number" ADD CONSTRAINT "phone_number_wabaId_fkey" FOREIGN KEY ("wabaId") REFERENCES "whatsapp_business_account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
