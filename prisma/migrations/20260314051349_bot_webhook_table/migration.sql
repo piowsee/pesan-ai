6598001736ee743e0215894de544e15d7d220fa0
WilsonnnTan <wilsonangelietann@gmail.com>
2026-03-17 19:23:29 +0700

    refactor: add passphrase field on botwebhook table

--- prisma/migrations/20260314051349_bot_webhook_table/migration.sql
+++ prisma/migrations/20260314051349_bot_webhook_table/migration.sql
@@ -6,6 +6,7 @@ CREATE TABLE "bot_webhook" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "webhookUrl" TEXT NOT NULL,
+    "passphrase" TEXT NOT NULL,
     "description" TEXT,
     "isActive" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
