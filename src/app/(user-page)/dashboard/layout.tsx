6a3ead26ffdf46201cdfb9784ca85415798b3d97
WilsonnnTan <wilsonangelietann@gmail.com>
2026-03-21 18:04:40 +0700

    docs: add note for future implementation

--- src/app/(user-page)/dashboard/layout.tsx
+++ src/app/(user-page)/dashboard/layout.tsx
@@ -7,6 +7,9 @@ import {
 import { requireUser } from '@/lib/auth/auth-page-helper';
 import { ReactNode } from 'react';
 
+// NOTE: Layout server data won't auto-update on client navigation.
+// Call `router.refresh()` or other event trigger for layout
+// after any user profile update to re-run server code and get fresh session data.
 export default async function DashboardLayout({
   children,
 }: {
