e52b304261160eaeb553ddffb6223c6262883a05
WilsonnnTan <wilsonangelietann@gmail.com>
2026-06-29 23:06:23 +0700

    feat: init s3 client

---
+++ src/lib/server/s3-client.ts
@@ -0,0 +1,23 @@
+import { S3Client } from '@aws-sdk/client-s3';
+
+function requiredEnv(name: string): string {
+  const value = process.env[name];
+
+  if (!value) {
+    throw new Error(`Missing required env var: ${name}`);
+  }
+
+  return value;
+}
+
+const s3Client = new S3Client({
+  forcePathStyle: false,
+  endpoint: requiredEnv('DIGITAL_OCEAN_SPACES_BUCKET_ENDPOINT'),
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: requiredEnv('DIGITAL_OCEAN_SPACES_BUCKET_ACCESS_KEY'),
+    secretAccessKey: requiredEnv('DIGITAL_OCEAN_SPACES_BUCKET_SECRET_KEY'),
+  },
+});
+
+export { s3Client };
